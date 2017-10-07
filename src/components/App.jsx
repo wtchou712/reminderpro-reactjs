76299567f811e9be5e8c1c24dcbe68d3f9e148da
William Chou <wtchou712@gmail.com>
2017-10-06 21:03:41 -0700

    browser cookie and clear reminders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder, deleteReminder } from '../actions';
+import { addReminder, deleteReminder, clearReminders } from '../actions';
 import moment from 'moment';
 
 class App extends Component {
@@ -75,6 +75,12 @@ class App extends Component {
 					</button>
 				</div>
 				{ this.renderReminders() }
+				<div
+					className="btn btn-danger"
+					onClick={()=> this.props.clearReminders()}
+				>
+					Clear Reminders
+				</div>
 			</div> 
 		)
 	}
@@ -87,4 +93,4 @@ function mapStateToProps(state){
 	}
 }
 
-export default connect(mapStateToProps, { addReminder, deleteReminder })(App); 
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App); 
